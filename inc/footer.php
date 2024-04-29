67c273c5073327a5a8f7f1a05787f2a83bb358ff
Timilsinaashim <ashimtimilsina052@gmail.com>
2024-04-29 22:44:35 +0545

    cloudfront added

--- inc/footer.php
+++ inc/footer.php
@@ -80,33 +80,36 @@
       $.widget.bridge('uibutton', $.ui.button)
     </script>
     <!-- Bootstrap 4 -->
-    <script src="<?php echo base_url ?>plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- ChartJS -->
-    <script src="<?php echo base_url ?>plugins/chart.js/Chart.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/chart.js/Chart.min.js"></script>
     <!-- Sparkline -->
-    <script src="<?php echo base_url ?>plugins/sparklines/sparkline.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/sparklines/sparkline.js"></script>
     <!-- Select2 -->
-    <script src="<?php echo base_url ?>plugins/select2/js/select2.full.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/select2/js/select2.full.min.js"></script>
     <!-- JQVMap -->
-    <script src="<?php echo base_url ?>plugins/jqvmap/jquery.vmap.min.js"></script>
-    <script src="<?php echo base_url ?>plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/jqvmap/jquery.vmap.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
     <!-- jQuery Knob Chart -->
-    <script src="<?php echo base_url ?>plugins/jquery-knob/jquery.knob.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/jquery-knob/jquery.knob.min.js"></script>
     <!-- daterangepicker -->
-    <script src="<?php echo base_url ?>plugins/moment/moment.min.js"></script>
-    <script src="<?php echo base_url ?>plugins/daterangepicker/daterangepicker.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/moment/moment.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/daterangepicker/daterangepicker.js"></script>
     <!-- Tempusdominus Bootstrap 4 -->
-    <script src="<?php echo base_url ?>plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
     <!-- Summernote -->
-    <script src="<?php echo base_url ?>plugins/summernote/summernote-bs4.min.js"></script>
-    <script src="<?php echo base_url ?>plugins/datatables/jquery.dataTables.min.js"></script>
-    <script src="<?php echo base_url ?>plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
-    <script src="<?php echo base_url ?>plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
-    <script src="<?php echo base_url ?>plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/summernote/summernote-bs4.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
     <!-- overlayScrollbars -->
-    <!-- <script src="<?php echo base_url ?>plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
+    <!-- <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script> -->
+    
     <!-- AdminLTE App -->
-    <script src="<?php echo base_url ?>dist/js/adminlte.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/dist/js/adminlte.js"></script>
+
+
   </div>   
     <div class="daterangepicker ltr show-ranges opensright">
       <div class="ranges">
--- inc/header.php
+++ inc/header.php
@@ -15,50 +15,57 @@
     <!-- Google Font: Source Sans Pro -->
     <!-- <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&amp;display=fallback"> -->
     <!-- Font Awesome -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/fontawesome-free/css/all.min.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/fontawesome-free/css/all.min.css">
     <!-- Ionicons -->
     <!-- <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css"> -->
     <!-- Tempusdominus Bootstrap 4 -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
       <!-- DataTables -->
-  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?php echo base_url ?>plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
    <!-- Select2 -->
-  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2/css/select2.min.css">
-  <link rel="stylesheet" href="<?php echo base_url ?>plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+  <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/select2/css/select2.min.css">
+  <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
     <!-- iCheck -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
     <!-- JQVMap -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/jqvmap/jqvmap.min.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/jqvmap/jqvmap.min.css">
+
+
+
     <!-- Theme style -->
-    <link rel="stylesheet" href="<?php echo base_url ?>dist/css/adminlte.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>dist/css/custom.css">
-    <link rel="stylesheet" href="<?php echo base_url ?>assets/css/styles.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/dist/css/adminlte.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/dist/css/custom.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/assets/css/styles.css">
+
+
     <!-- overlayScrollbars -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
     <!-- Daterange picker -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/daterangepicker/daterangepicker.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/daterangepicker/daterangepicker.css">
     <!-- summernote -->
-    <link rel="stylesheet" href="<?php echo base_url ?>plugins/summernote/summernote-bs4.min.css">
+    <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/summernote/summernote-bs4.min.css">
      <!-- SweetAlert2 -->
-  <link rel="stylesheet" href="<?php echo base_url ?>plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
+  <link rel="stylesheet" href="https://d282e2u7vt72l0.cloudfront.net/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
     <style type="text/css">/* Chart.js */
       @keyframes chartjs-render-animation{from{opacity:.99}to{opacity:1}}.chartjs-render-monitor{animation:chartjs-render-animation 1ms}.chartjs-size-monitor,.chartjs-size-monitor-expand,.chartjs-size-monitor-shrink{position:absolute;direction:ltr;left:0;top:0;right:0;bottom:0;overflow:hidden;pointer-events:none;visibility:hidden;z-index:-1}.chartjs-size-monitor-expand>div{position:absolute;width:1000000px;height:1000000px;left:0;top:0}.chartjs-size-monitor-shrink>div{position:absolute;width:200%;height:200%;left:0;top:0}
     </style>
 
      <!-- jQuery -->
-    <script src="<?php echo base_url ?>plugins/jquery/jquery.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/jquery/jquery.min.js"></script>
     <!-- jQuery UI 1.11.4 -->
-    <script src="<?php echo base_url ?>plugins/jquery-ui/jquery-ui.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/jquery-ui/jquery-ui.min.js"></script>
     <!-- SweetAlert2 -->
-    <script src="<?php echo base_url ?>plugins/sweetalert2/sweetalert2.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/sweetalert2/sweetalert2.min.js"></script>
     <!-- Toastr -->
-    <script src="<?php echo base_url ?>plugins/toastr/toastr.min.js"></script>
+    <script src="https://d282e2u7vt72l0.cloudfront.net/plugins/toastr/toastr.min.js"></script>
     <script>
-        var _base_url_ = '<?php echo base_url ?>';
+        var _base_url_ = 'https://d282e2u7vt72l0.cloudfront.net/';
     </script>
-    <script src="<?php echo base_url ?>dist/js/script.js"></script>
+
+
+    <script src="https://d282e2u7vt72l0.cloudfront.net/dist/js/script.js"></script>
     <style>
     #main-header{
         position:relative;
