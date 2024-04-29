890608a9d211a8650a54a9b78e7045ad4b43a1a1
Timilsinaashim <ashimtimilsina052@gmail.com>
2024-04-29 14:59:25 +0545

    first commit

--- initialize.php
+++ initialize.php
@@ -4,16 +4,16 @@ if(!defined('base_url')) define('base_url','http://localhost/cdcms');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
 
-// php test
-// if(!defined('DB_SERVER')) define('DB_SERVER',"project-php.ca6crwpmxkgt.us-east-1.rds.amazonaws.com");
-//  if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
-//  if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"adminadmin");
-// if(!defined('DB_NAME')) define('DB_NAME',"cdcms_db");
+php test
+if(!defined('DB_SERVER')) define('DB_SERVER',"project-php.ca6crwpmxkgt.us-east-1.rds.amazonaws.com");
+ if(!defined('DB_USERNAME')) define('DB_USERNAME',"admin");
+ if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"adminadmin");
+if(!defined('DB_NAME')) define('DB_NAME',"cdcms");
 
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
- if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
- if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"cdcms_db");
+// if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
+//  if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
+//  if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
+// if(!defined('DB_NAME')) define('DB_NAME',"cdcms_db");
 
 ?>
 
