8e876156e3ff3231023f1df849561a94f7d03224
Timilsinaashim <ashimtimilsina052@gmail.com>
2024-04-29 15:07:20 +0545

    first commit

--- initialize.php
+++ initialize.php
@@ -10,9 +10,10 @@ if(!defined('DB_SERVER')) define('DB_SERVER',"project-php.ca6crwpmxkgt.us-east-1
  if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"adminadmin");
 if(!defined('DB_NAME')) define('DB_NAME',"cdcms");
 
-?>
-
 // if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
 //  if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
 //  if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
 // if(!defined('DB_NAME')) define('DB_NAME',"cdcms_db");
+?>
+
+
